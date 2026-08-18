aa43b4fd6d068514dbe69028ae804d859e2d7046
DeviSri-2008 <devisri190508@gmail.com>
2026-08-18 15:38:16 +0530

    Add files via upload

---
+++ Marks.java
@@ -0,0 +1,24 @@
+public class Marks{
+	public static void main(String[]args){
+		int Marks;
+		Marks = 40;
+		if(Marks>=90)
+		{
+			System.out.println("A+");
+		}
+		else if(Marks>80)
+		{
+			System.out.println("A");
+		}
+		else if(Marks>70)
+		{
+			System.out.println("B");
+		}
+		else
+		{
+			System.out.println("Fail");
+		}
+	}
+}
+
+	
